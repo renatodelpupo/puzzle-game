d5bd1dac87e6994d25975909ad6fb5fe3ee07623
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-04-11 19:37:34 -0300

    Update config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,15 @@
-index: path.resolve(__dirname, '../gh-pages/index.html')
-assetsRoot: path.resolve(__dirname, '../gh-pages')
-assetsPublicPath: ''
+var path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../gh-pages/index.html'),
+    assetsRoot: path.resolve(__dirname, '../gh-pages'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/puzzle-game/',
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report
+  }
+}
