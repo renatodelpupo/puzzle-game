887ebbb4e99f889fe86e2ad4159ad048a69388ae
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-04-12 18:26:41 -0300

    Creating a simple icon representation

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    msTileColor: '#383838',
+    name: 'Solve the puzzle',
+    themeColor: '#383838'
+  }
+}
