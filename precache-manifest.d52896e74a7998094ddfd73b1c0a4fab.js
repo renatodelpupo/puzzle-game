6dc00573d1c6f64e02c54eadbd43fd088141c640
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-04-26 01:15:56 -0300

    Build

--- precache-manifest.d52896e74a7998094ddfd73b1c0a4fab.js
+++ precache-manifest.d52896e74a7998094ddfd73b1c0a4fab.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcffd148fec7642cba27",
-    "url": "/css/app.911a8bfd.css"
+    "revision": "eb94e22ddc85500eadfb",
+    "url": "/css/app.5f9db8dd.css"
   },
   {
-    "revision": "41fd8a3d9f6a9aaf5f329ffb205ff433",
+    "revision": "18785d1f22b5468c942513f412c1b013",
     "url": "/index.html"
   },
   {
-    "revision": "dcffd148fec7642cba27",
-    "url": "/js/app.50eea2dc.js"
+    "revision": "eb94e22ddc85500eadfb",
+    "url": "/js/app.f9703384.js"
   },
   {
     "revision": "d4c0693a89444461084a",
     "url": "/js/chunk-vendors.258890e8.js"
   },
   {
-    "revision": "ef3361af913691dd27d25c13730413b3",
+    "revision": "ba42f8db286d0baa306d11e594f38910",
     "url": "/manifest.json"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9f5b7f67bcd9a96c31a5644aa81c4b3.js"
+  "/precache-manifest.d52896e74a7998094ddfd73b1c0a4fab.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "solve-the-puzzle"});
+workbox.core.setCacheNameDetails({prefix: "solve-it"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
