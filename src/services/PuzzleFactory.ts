333d74533e141e96f0e6fdbea062ab67b7dfca1a
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-08-22 16:39:28 -0300

    feat: avoid similar rules

--- src/services/PuzzleFactory.ts
+++ src/services/PuzzleFactory.ts
@@ -16,14 +16,46 @@ export const generateRules = (rulesLength: number, rulesQuantity: number): Array
     return uniqueNumbers
   }
 
+  const createRulesBaseKeys = (): Array<Array<number>> => {
+    const rulesBaseKeys: Array<Array<number>> = []
+
+    const newNumbersKeyIsUnique = (newNumbersKey: Array<number>) => {
+      rulesBaseKeys.forEach((rule) => {
+        if (!rulesBaseKeys.length) return true
+
+        const equalNumbers = rule.filter((number: number) => newNumbersKey.includes(number))
+        const correctNumbers = equalNumbers.length
+
+        if (correctNumbers === rulesLength) {
+          return false
+        }
+      })
+
+      return true
+    }
+
+    // eslint-disable-next-line space-in-parens
+    for (let i = 0; i < rulesQuantity; ) {
+      const newNumbersKey = createUniqueNumbersKey()
+
+      if (newNumbersKeyIsUnique(newNumbersKey)) {
+        rulesBaseKeys.push(newNumbersKey)
+        i++
+      }
+    }
+
+    return rulesBaseKeys
+  }
+
   const createRulesBase = (): Array<Rule> => {
+    const rulesBaseKeys: Array<Array<number>> = createRulesBaseKeys()
     const rules: Array<Rule> = []
 
     for (let i = 0; i < rulesQuantity; i++) {
       rules.push({
         correctNumbers: 0,
         correctPositions: 0,
-        numbers: createUniqueNumbersKey()
+        numbers: rulesBaseKeys[i]
       })
     }
 
