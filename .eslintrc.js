2dfff1729bbfe043c4c57f6b2b601a57393dc722
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-09-25 15:15:14 -0300

    build: install jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,29 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript'],
+
   parserOptions: {
     parser: '@typescript-eslint/parser'
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': 'off',
     'space-before-function-paren': ['error', 'never']
-  }
+  },
+
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
