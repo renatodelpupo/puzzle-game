74bb95ca40876c2bffc3ceed7cbe4eaef6bcb493
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-01-12 16:06:34 -0300

    chore: move texts to i18n

--- src/components/atoms/Rule.vue
+++ src/components/atoms/Rule.vue
@@ -23,22 +23,22 @@ export default {
   methods: {
     setDescription(rule: RuleInterface): string {
       const descriptionNumbers = () => {
-        if (rule.correctNumbers === 0) return 'No correct numbers.'
-        if (rule.correctNumbers === 1) return 'One correct number'
-        if (rule.correctNumbers === 2) return 'Two correct numbers'
-        if (rule.correctNumbers === 3) return 'Three correct numbers'
-        if (rule.correctNumbers === 4) return 'Four correct numbers'
+        if (rule.correctNumbers === 0) return this.$t('correctNumbers0')
+        if (rule.correctNumbers === 1) return this.$t('correctNumbers1')
+        if (rule.correctNumbers === 2) return this.$t('correctNumbers2')
+        if (rule.correctNumbers === 3) return this.$t('correctNumbers3')
+        if (rule.correctNumbers === 4) return this.$t('correctNumbers4')
 
         return ''
       }
 
       const descriptionPositions = () => {
         if (!rule.correctNumbers) return ''
-        if (rule.correctPositions === rule.correctNumbers) return ' and in the right place.'
-        if (rule.correctPositions === 0) return ', but in the wrong place.'
-        if (rule.correctPositions === 1) return ', but only one in the right place.'
-        if (rule.correctPositions === 2) return ', but only two in the right place.'
-        if (rule.correctPositions === 3) return ', but only three in the right place.'
+        if (rule.correctPositions === rule.correctNumbers) return this.$t('correctPositionsAll')
+        if (rule.correctPositions === 0) return this.$t('correctPositions0')
+        if (rule.correctPositions === 1) return this.$t('correctPositions1')
+        if (rule.correctPositions === 2) return this.$t('correctPositions2')
+        if (rule.correctPositions === 3) return this.$t('correctPositions3')
 
         return ''
       }
@@ -48,6 +48,23 @@ export default {
   }
 }
 </script>
+
+<i18n>
+{
+  "en": {
+    "correctNumbers0": "No correct numbers.",
+    "correctNumbers1": "One correct number",
+    "correctNumbers2": "Two correct numbers",
+    "correctNumbers3": "Three correct numbers",
+    "correctNumbers4": "Four correct numbers",
+    "correctPositions0": ", but in the wrong place.",
+    "correctPositions1": ", but only one in the right place.",
+    "correctPositions2": ", but only two in the right place.",
+    "correctPositions3": ", but only three in the right place.",
+    "correctPositionsAll": " and in the right place."
+  }
+}
+</i18n>
 
 <style lang="scss" scoped>
 .rule {
