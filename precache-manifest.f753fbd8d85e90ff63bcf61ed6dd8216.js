d4cf84981787e43916af8e6de3c3af582beae0a1
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-01-13 10:21:25 -0300

    Build

--- precache-manifest.f753fbd8d85e90ff63bcf61ed6dd8216.js
+++ precache-manifest.f753fbd8d85e90ff63bcf61ed6dd8216.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ca7a6064d2f5e8a9b271",
-    "url": "/css/app.18a1b86e.css"
+    "url": "/puzzle-game/css/app.18a1b86e.css"
   },
   {
     "revision": "347c51d7c5ca33c8d8a3474e43f27724",
-    "url": "/index.html"
+    "url": "/puzzle-game/index.html"
   },
   {
     "revision": "ca7a6064d2f5e8a9b271",
-    "url": "/js/app.0ad17bc9.js"
+    "url": "/puzzle-game/js/app.0ad17bc9.js"
   },
   {
     "revision": "3831e1e63e707a5291e7",
-    "url": "/js/chunk-vendors.e7a6cef4.js"
+    "url": "/puzzle-game/js/chunk-vendors.e7a6cef4.js"
   },
   {
     "revision": "ba42f8db286d0baa306d11e594f38910",
-    "url": "/manifest.json"
+    "url": "/puzzle-game/manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "/puzzle-game/robots.txt"
   }
 ]);
