554704aa56118d0409831393053faf8f6f1ece01
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-04-29 13:50:32 -0300

    chore: add method set key test

--- tests/unit/Rules.spec.js
+++ tests/unit/Rules.spec.js
@@ -1,12 +1,12 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 
 import { createRules } from '@/services/PuzzleFactory'
 import Rule from '@/components/atoms/Rule.vue'
 import Rules from '@/components/organisms/Rules.vue'
 
-describe('Rules.vue', () => {
+describe('mount', () => {
   it('all rules rendered', () => {
-    const wrapper = mount(Rules, {
+    const wrapper = shallowMount(Rules, {
       propsData: {
         rules: createRules(3, 6)
       }
@@ -16,5 +16,20 @@ describe('Rules.vue', () => {
     const renderedRulesLength = wrapper.findAllComponents(Rule).length
 
     expect(passedRulesLength).toBe(renderedRulesLength)
+  })
+})
+
+describe('methods', () => {
+  test('set key', () => {
+    const wrapper = shallowMount(Rules, {
+      propsData: {
+        rules: createRules(3, 6)
+      }
+    })
+    const key = wrapper.vm.setKey({
+      numbers: [1, 2, 3]
+    })
+
+    expect(key).toBe('123')
   })
 })
